2d365120ccdaa0d6b3e90c9a3b75cd94cade76c1
Mukulika Dey <muku.dey27@gmail.com>
2018-11-23 16:02:09 -0500

    modified spider

--- concordiacrawler/concordiacrawler/spiders/concordiaSpider.py
+++ concordiacrawler/concordiacrawler/spiders/concordiaSpider.py
@@ -5,30 +5,23 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class concordiaSpider(scrapy.Spider):
     name = "concordia_spider"
-
-    def start_requests(self):
-        # COUNT_MAX = 10
-        
-        # custom_settings = {
-        #     'CLOSESPIDER_PAGECOUNT': COUNT_MAX
-        # }
-
-        allowed_domains = ["concordia.ca"]
-        start_urls = ["https://www.concordia.ca/about.html"]
-
-        # rules = (
-        #     Rule(LinkExtractor(), callback=self.parse, follow=True),
-        # )
-
-        for url in start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = ["https://www.concordia.ca/about.html"]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
+        print("Printing Response")
+        print(response)
 
-        filename = 'concordia-%s.html' % page
+        link_extractor = LinkExtractor()
+        links = link_extractor.extract_links(response)
 
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for link in links:
+            print(link.url)
 
-        self.log('Saved file %s' % filename)
+            page = response.url.split("/")[-2]
+
+            filename = 'concordia-%s.html' % page
+
+            with open(filename, 'wb') as f:
+                f.write(response.body)
+
+            self.log('Saved file %s' % filename)
