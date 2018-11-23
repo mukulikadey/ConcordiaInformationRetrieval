f2770b5312f360f883d47fd57ad8c4a182ccbf66
Radu Raicea <radu.raicea@shopify.com>
2018-11-23 15:15:07 -0500

    Fix

--- concordiacrawler/concordiacrawler/spiders/concordiaSpider.py
+++ concordiacrawler/concordiacrawler/spiders/concordiaSpider.py
@@ -5,27 +5,30 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class concordiaSpider(scrapy.Spider):
     name = "concordia_spider"
-    COUNT_MAX = 10
 
-    custom_settings = {
-        'CLOSESPIDER_PAGECOUNT': COUNT_MAX
-    }
+    def start_requests(self):
+        # COUNT_MAX = 10
+        
+        # custom_settings = {
+        #     'CLOSESPIDER_PAGECOUNT': COUNT_MAX
+        # }
 
-    allowed_domains = ["concordia.ca"]
-    start_urls = ["https://www.concordia.ca/about.html,"]
+        allowed_domains = ["concordia.ca"]
+        start_urls = ["https://www.concordia.ca/about.html"]
 
-    rules = (
-        Rule(LinkExtractor(), callback=self.parse, follow=True),
-    )
+        # rules = (
+        #     Rule(LinkExtractor(), callback=self.parse, follow=True),
+        # )
+
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        yield scrapy.Request(response, callable('parse'))
         page = response.url.split("/")[-2]
+
         filename = 'concordia-%s.html' % page
+
         with open(filename, 'wb') as f:
             f.write(response.body)
+
         self.log('Saved file %s' % filename)
-
-
-
-
