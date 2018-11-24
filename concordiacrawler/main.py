4da1af2b28bf7735f6d5427e9a8cfff33795ecc3
Mukulika Dey <muku.dey27@gmail.com>
2018-11-24 17:49:11 -0500

    Added the preprocessing and spimi files

--- concordiacrawler/main.py
+++ concordiacrawler/main.py
@@ -1,9 +1,39 @@
 import json
 import get_content
+import preprocessing
+from spimi import SPIMI
+
+def dump_bm25_param(documents):
+    num_documents = len(documents.keys())
+    doc_len = {}
+    sum_len = 0
+
+    for docId, terms_list in documents.items():
+        length = len(terms_list)
+        doc_len[docId] = length
+        sum_len += length
+
+    avg_len = sum_len / num_documents
+
+    bm25_param = {
+        'num_documents': num_documents,
+        'doc_len': doc_len,
+        'avg_len': avg_len
+    }
+
+    with open('bm25_param.txt', 'w') as f:
+        json.dump(bm25_param, f)
 
 if __name__ == '__main__':
 
+    block_size_limit = 500
     print("=============== Retriving documents =============== ")
     documents = get_content.getDocuments()
-    print(documents)
-
+    # print(documents)
+    print("=============== Preprocessing documents ===========")
+    documents = preprocessing.preprocess(documents)
+    dump_bm25_param(documents)
+    # print(documents)
+    print("=============== Applying SPIMI Algorithm ==========")
+    spimi = SPIMI(block_size_limit, documents)
+    spimi.invert()
---
+++ concordiacrawler/preprocessing.py
@@ -0,0 +1,29 @@
+import nltk
+import string
+from nltk.corpus import stopwords
+
+def preprocess(documents):
+    # Tokenize each word in documents
+    for key in documents.keys():
+        tokens = nltk.word_tokenize((documents[key]))
+        processed_tokens = normalize(tokens)
+        documents[key] = processed_tokens
+
+    return documents
+
+
+def normalize(tokens):
+
+    processed_tokens = tokens
+    # -- 1. Discard Tokens with punctuation marks
+    processed_tokens = [token for token in processed_tokens if not token in string.punctuation]
+    # -- 2. Discard blank tokens
+    processed_tokens = filter(None, processed_tokens)
+    processed_tokens = [token for token in processed_tokens if not token == "''" and not token == '``']
+    # -- 3. Discard tokens that are digits
+    processed_tokens = [token for token in processed_tokens if not token.isdigit()]
+    processed_tokens = [token for token in processed_tokens if token.isalpha()]
+    # -- 4. Apply lowercase to all tokens
+    processed_tokens = [token.lower() for token in processed_tokens]
+
+    return processed_tokens
